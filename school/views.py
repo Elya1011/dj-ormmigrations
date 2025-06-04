8d6e53d91ad4e77482b04e5e2da83e4620f2a41f
Elya1011 <elviraalimova06@gmail.com>
2025-06-05 01:02:13 +0300

    final commit

--- school/views.py
+++ school/views.py
@@ -7,14 +7,11 @@ from .models import Student, Teacher
 def students_list(request):
     template = 'school/students_list.html'
     context = {}
-    for student in Student.objects.all():
-        if student.teacher:
-            student.teachers.add(student.teachers)
+
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
     ordering = 'group'
-    # students = Student.objects.all()
-    students = Student.objects.prefetch_related('teachers')
+    students = Student.objects.prefetch_related('teachers').order_by(ordering)
     context['students'] = students
 
     return render(request, template, context)
